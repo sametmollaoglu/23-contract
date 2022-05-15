9fe80ba6a50be88af5af6b32c4ba969ffec54113
sametmollaoglu <samet.mollaoglu85@gmail.com>
2022-05-15 16:33:38 +0300

    last_commit

--- assembly/model.ts
+++ assembly/model.ts
@@ -1,4 +1,4 @@
-import { u128, context, PersistentMap, math } from "near-sdk-as";
+import { u128, context, PersistentMap, math, storage} from "near-sdk-as";
 
 export const games = new PersistentMap<u32, game_23>("game");
 
@@ -30,6 +30,14 @@ export class game_23 {
         this.gameId = math.hash32<string>(context.sender);
         this.bet = context.attachedDeposit;
         this.readyToStart= false;
+
+        storage.set<u32>("gameId", this.gameId);
     }
+
+    
+    static getGameId(): u32 {
+        const storedGameId = storage.getPrimitive<u32>("gameId", 0);
+        return storedGameId;
+      }
 }
 
